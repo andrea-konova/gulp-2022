c24d2311113c713d00e7fa4efef08ca1ef00cd7a
Andrii Konovalenko <an256.ak@gmail.com>
2022-05-11 23:48:29 +0300

    feat: add html

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,31 @@
 import gulp from "gulp";
 // импорт путей
 import { path } from "./gulp/config/path.js";
+// импорт общих плагинов
+import { plugins } from "./gulp/config/plugins.js";
 
 // передаем значения в глобальную переменную
 global.app = {
   path: path,
-  gulp: gulp
+  gulp: gulp,
+  plugins: plugins
 }
 
 // импорт задач
 import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+
+// наблюдатель за изменениями в файле
+function watcher() {
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+}
+
+const mainTasks = gulp.parallel(copy, html);
+
+// построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks, watcher);
 
 // выполненеие сценария по умолчанию
-gulp.task('default', copy);
+gulp.task('default', dev);
