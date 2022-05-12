4bf52baf797c706cc04117fd1dea20509a13e8d9
Andrii Konovalenko <an256.ak@gmail.com>
2022-05-12 19:11:02 +0300

    feat: add html task

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,5 +1,9 @@
-import replace from "gulp-replace";
+import replace from "gulp-replace"; // поиск и замена
+import plumber from "gulp-plumber"; // обработка ошибок
+import notify from "gulp-notify"; // сообщения (подсказки)
 
 export const plugins = {
   replace: replace,
+  plumber: plumber,
+  notify: notify
 }
