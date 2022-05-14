c8df1ac87db0816c4afafe1a2f28f144ee0dad0c
Andrii Konovalenko <an256.ak@gmail.com>
2022-05-14 11:10:05 +0300

    feat: add ftp task

--- gulpfile.js
+++ gulpfile.js
@@ -25,11 +25,12 @@ import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 import { svgSprive } from "./gulp/tasks/svgSprive.js";
 import { zip } from "./gulp/tasks/zip.js";
+import { ftp } from "./gulp/tasks/ftp.js";
 
 // наблюдатель за изменениями в файле
 function watcher() {
   gulp.watch(path.watch.files, copy);
-  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.html, html); // gulp.series(html, ftp)
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
   gulp.watch(path.watch.images, images);
@@ -46,11 +47,13 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
 
 // экспорт сценариев
 export { dev }
 export { build }
 export { deployZIP }
+export { deployFTP }
 
 // выполненеие сценария по умолчанию
 gulp.task('default', dev);
