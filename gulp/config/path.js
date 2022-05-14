665d472463c8c0294424809f77e84a6a57ba6aef
Andrii Konovalenko <an256.ak@gmail.com>
2022-05-14 08:50:49 +0300

    feat: add fonts task

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -11,6 +11,7 @@ export const path = {
     css: `${buildFolder}/css/`,
     images: `${buildFolder}/img/`,
     html: `${buildFolder}/`,
+    fonts: `${buildFolder}/fonts/`,
     files: `${buildFolder}/files/`
   },
   src: {
--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 
 // наблюдатель за изменениями в файле
 function watcher() {
@@ -30,7 +31,11 @@ function watcher() {
   gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss, js, images);
+// последовательная обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+// основные задачи
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 // построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
